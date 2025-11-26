b3b48740f8c9dcffb1799e92350b2c752c92e5a1
Shawiiz_z <shaweereso@gmail.com>
2025-11-26 22:04:47 +0100

    feat: docs

---
+++ docs/app/layout.tsx
@@ -0,0 +1,50 @@
+import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
+import 'nextra-theme-docs/style.css'
+
+export const metadata = {
+  title: {
+    template: '%s - Dockflow',
+    default: 'Dockflow Documentation'
+  },
+  description: 'Documentation for Dockflow',
+  applicationName: 'Dockflow',
+  generator: 'Next.js',
+  appleWebApp: {
+    title: 'Dockflow'
+  },
+  other: {
+    'msapplication-TileColor': '#fff'
+  },
+  twitter: {
+    site: 'https://dockflow.org'
+  }
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const navbar = (
+    <Navbar
+      logo={<b>Dockflow</b>}
+      projectLink="https://github.com/Shawiizz/dockflow"
+    />
+  )
+  const footer = <Footer>Dockflow Documentation</Footer>
+
+  return (
+    <html lang="en" dir="ltr" suppressHydrationWarning>
+      <Head />
+      <body>
+        <Layout
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/Shawiizz/dockflow/tree/main/docs"
+          footer={footer}
+          sidebar={{ defaultMenuCollapseLevel: 1 }}
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  )
+}
