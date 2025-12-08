46a396dc5077c21d8ac47384d9b817428ccc557f
Shawiiz_z <shaweereso@gmail.com>
2025-12-08 21:50:02 +0100

    wip: add docker swarm support

--- docs/app/configuration/_meta.ts
+++ docs/app/configuration/_meta.ts
@@ -3,6 +3,7 @@ export default {
   environment: 'Environment Files',
   'docker-compose': 'Docker Compose',
   'build-strategy': 'Build Strategy',
+  registry: 'Docker Registry',
   'multi-host': 'Multi-Host Deployment',
   'config-file': 'Configuration File'
 }
