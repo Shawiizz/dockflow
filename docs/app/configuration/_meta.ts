4e04ec7faaa64afd7c04e49cb3c232ef61d063bd
Shawiiz_z <shaweereso@gmail.com>
2025-12-12 14:01:47 +0100

    feat: hooks

--- docs/app/configuration/_meta.ts
+++ docs/app/configuration/_meta.ts
@@ -4,6 +4,7 @@ export default {
   'docker-compose': 'Docker Compose',
   'build-strategy': 'Build Strategy',
   registry: 'Docker Registry',
+  hooks: 'Hooks',
   'multi-host': 'Multi-Host Deployment',
   'config-file': 'Configuration File'
 }
