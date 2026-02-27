7627f7c67331e2816572a7e4e84381bf0584ea1d
Shawiiz_z <shaweereso@gmail.com>
2026-02-27 14:46:23 +0100

    release: 2.0.20

--- install.ps1
+++ install.ps1
@@ -4,7 +4,7 @@
 $ErrorActionPreference = "Stop"
 
 # Version to install
-$Version = "2.0.19"
+$Version = "2.0.20"
 
 # Detect architecture
 $arch = if ([Environment]::Is64BitOperatingSystem) { "x64" } else { "x86" }
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Version to install
-VERSION="2.0.19"
+VERSION="2.0.20"
 
 # Detect OS and architecture
 OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
